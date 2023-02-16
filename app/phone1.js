6135d244850862d136417c66124052436e59145f
Enamul <muhammad.enamul.haque79@gmail.com>
2023-02-16 12:10:14 +0600

    first commit

---
+++ app/phone1.js
@@ -0,0 +1,63 @@
+//update phone number function;
+
+function updatePhoneNumber(isIncrease){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+    let newPhoneNumber;
+
+    if(isIncrease === true){
+        newPhoneNumber = previousPhoneNumber + 1;
+    }else{
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+   return phoneNumberField.value = newPhoneNumber;
+}
+
+//Total price update function;
+
+function updatePhoneTotalPrice(newPhoneNumber){
+    const phoneTotalPrice = newPhoneNumber * 1219;
+   const phoneTotalElement = document.getElementById('phone-total');
+   phoneTotalElement.innerText = phoneTotalPrice;
+};
+
+document.getElementById('btn-phone-plus').addEventListener('click',function(){
+   const newPhoneNumber = updatePhoneNumber(true);
+    updatePhoneTotalPrice(newPhoneNumber);
+});
+// document.getElementById('btn-phone-plus').addEventListener('click',function(){
+//    const newPhoneNumber = updatePhoneNumber(true);
+//    const phoneTotalPrice = newPhoneNumber * 1219;
+//    const phoneTotalElement = document.getElementById('phone-total');
+//    phoneTotalElement.innerText = phoneTotalPrice;
+//     
+// });
+
+
+document.getElementById('btn-phone-minus').addEventListener('click',function(){
+  const newPhoneNumber = updatePhoneNumber(false);
+  updatePhoneTotalPrice(newPhoneNumber);
+
+});
+
+
+// document.getElementById('btn-phone-plus').addEventListener('click',function(){
+//     const phoneNumberField = document.getElementById('phone-number-field');
+//     const phoneNumberString = phoneNumberField.value;
+//     const previousPhoneNumber = parseInt(phoneNumberString);
+//     const newPhoneNumber = previousPhoneNumber + 1;
+//     phoneNumberField.value = newPhoneNumber;
+    
+// });
+
+
+// document.getElementById('btn-phone-minus').addEventListener('click',function(){
+//     const phoneNumberField = document.getElementById('phone-number-field');
+//     const phoneNumberString = phoneNumberField.value;
+//     const previousPhoneNumber = parseInt(phoneNumberString);
+//     const newPhoneNumber = previousPhoneNumber - 1;
+//     phoneNumberField.value = newPhoneNumber;
+// });
+
+
